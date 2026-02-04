20f09b82cbf9f08155e179b570c80c20240b09bd
Craig McLean <craig.mclean@ensemble.com>
2026-02-04 12:45:09 -0500

    contact-card js/css

--- blocks/contact-cards/contact-cards.js
+++ blocks/contact-cards/contact-cards.js
@@ -1,26 +1,16 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
 import { moveInstrumentation } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement("ul");
+  const mainDiv = document.createElement("div");
   [...block.children].forEach((row) => {
-    const li = document.createElement("li");
+    const li = document.createElement("div");
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector("picture"))
-        div.className = "cards-card-image";
-      else div.className = "cards-card-body";
+      div.className = "contact-card";
     });
-    ul.append(li);
+    mainDiv.append(li);
   });
-  ul.querySelectorAll("picture > img").forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [
-      { width: "750" },
-    ]);
-    moveInstrumentation(img, optimizedPic.querySelector("img"));
-    img.closest("picture").replaceWith(optimizedPic);
-  });
-  block.replaceChildren(ul);
+
+  block.replaceChildren(mainDiv);
 }
