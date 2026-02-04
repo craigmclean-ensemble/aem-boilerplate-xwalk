7bbc647dc21943ab83d283a28fedd39f06c9775a
Craig McLean <craig.mclean@ensemble.com>
2026-02-04 10:32:48 -0500

    another test

--- blocks/location-cards/location-cards.js
+++ blocks/location-cards/location-cards.js
@@ -1,5 +1,5 @@
-export default function decorate(block) {
-  /* change to ul, li */
+/* export default function decorate(block) {
+
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
@@ -41,5 +41,33 @@ export default function decorate(block) {
     ul.append(li);
   });
 
+  block.replaceChildren(ul);
+}
+ */
+
+import { createOptimizedPicture } from "../../scripts/aem.js";
+import { moveInstrumentation } from "../../scripts/scripts.js";
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement("ul");
+  [...block.children].forEach((row) => {
+    const li = document.createElement("li");
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector("picture"))
+        div.className = "cards-card-image";
+      else div.className = "cards-card-body";
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll("picture > img").forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [
+      { width: "750" },
+    ]);
+    moveInstrumentation(img, optimizedPic.querySelector("img"));
+    img.closest("picture").replaceWith(optimizedPic);
+  });
   block.replaceChildren(ul);
 }
